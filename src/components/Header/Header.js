e56bd6cec1b8e363ff2f3d0313758cb810d270af
sohagmia360 <gdsohag360@gmail.com>
2022-04-04 08:28:09 +0600

    finish header and home and section reponsive and add hamberger menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FiMenu } from 'react-icons/fi';
 import { NavLink } from 'react-router-dom';
-
 const Header = () => {
+    const [showMenu , setShowMenu] = useState(true)
     return (
-        <div className='flex bg-white md:bg-transparent item-center justify-center w-full'>
-            <ul className='  navMenu flex   md:flex-row flex-col  items-center justify-center uppercase p-4 font-semibold tracking-wider text-primary_black'>
+        <div className='flex relative   md:bg-transparent item-center justify-center w-full'>
+             <h5 className='absolute z-50 text-4xl block md:hidden left-5 top-3'>
+                <FiMenu className='' />
+             </h5>   
+            <ul style={{transform: `translateY(${showMenu ? '0px' : '-250px'})`}} className=' bg-slate-200 md:bg-transparent shadow-md md:shadow-none w-full  navMenu flex   md:flex-row flex-col  items-center justify-center uppercase p-4 font-semibold tracking-wider text-primary_black'>
                 <li className='mx-2 md:mb-0 mb-4'>
                     <NavLink className={({isActive})=> isActive ? "text-orange_light   navItem before:bg-orange_light" : 'navItem'} to="/">Home</NavLink>
                 </li>
